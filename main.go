e7b87f40b5a0c2cad867203edd06a54c46ec3f5f
Mikhail Potapov <devslashnil@gmail.com>
2022-08-21 21:26:57 +0300

    added db connection

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/nats-io/stan.go"
+)
+
+// hardcode is bad, but in the task it's convenient for all...
+const (
+	host     = "localhost"
+	port     = 5432
+	username = "postgres"
+	password = "pass"
+	dbname   = "postgres"
+)
 
 func main() {
+	urlDb := fmt.Sprintf("postgres://%v:%v@%v:%v/%v", username,
+		password, host, port, dbname)
+	conn, err := pgx.Connect(context.Background(), urlDb)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
+	sc, err := stan.Connect("test-cluster", "pub")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to nats-streaming-server: %v\n", err)
+		os.Exit(1)
+	}
+	defer sc.Close()
 }
 
 type Order struct {
