ea2acd37df82751df7179a713854806d6a07dc58
Mikhail Potapov <devslashnil@gmail.com>
2022-08-25 14:33:15 +0300

    work on publisher

--- pub.go
+++ pub.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"reflect"
 
 	stan "github.com/nats-io/stan.go"
 )
@@ -43,9 +45,22 @@ func populateOrders(template Order, len int) []Order {
 	orders := make([]Order, 0)
 	for i := 0; i < len; i++ {
 		order := template
+
 		payment := template.Payment
 		delivery := template.Delivery
 		items := template.Items
 	}
 	return orders
+}
+
+func mutate(i any) {
+	v := reflect.ValueOf(i)
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		switch prop := field.Interface().(type) {
+		case int:
+			field.SetInt(int64(prop + rand.Intn(100)))
+		case string:
+		}
+	}
 }
